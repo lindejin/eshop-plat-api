3004f72a0d040d22a50ad096a92caed2e7bae299
yanzu <xixi>
2025-03-16 14:15:53 +0800

    1.删除多余代码

--- src/main/java/com/eshop/service/api/marketplace/biz/order/normal/infrastructure/temu/TemuPoOrderClient.java
+++
@@ -1,6 +0,0 @@
-package com.eshop.service.api.marketplace.biz.order.normal.infrastructure.temu;
-
-import com.eshop.api.modules.service.api.marketplace.order.domain.order.domain.repository.OrderClient;
-
-public interface TemuPoOrderClient extends OrderClient {
-}
