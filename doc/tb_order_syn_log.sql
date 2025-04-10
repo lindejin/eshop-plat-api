e747e70430655030e977a78b8d82358a07bbcc1b
ldj <1223042416@qq.com>
2025-04-10 18:05:58 +0800

    1.文件位置调整

--- doc/tb_order_syn_log.sql
+++ doc/tb_order_syn_log.sql
@@ -18,18 +18,6 @@ DELETE FROM `eshop_log`.`tb_order_syn_log`
 WHERE `exception_log` LIKE '%未支付%'
     LIMIT 10000;
 
-
-
-
-
-
-
-# 订单主表存在
-DELETE sol
-FROM eshop_log.tb_order_syn_log sol
-left JOIN eshop_order.tb_order o ON sol.order_no = o.order_no
-where o.order_no is not null;
-
 # 订单主表存在
 DELETE sol
 FROM eshop_log.tb_order_syn_log sol
