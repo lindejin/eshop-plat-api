456008491c05e5634c8bebc2fed1a9837663ce07
ldj <1223042416@qq.com>
2025-05-27 18:03:48 +0800

    doc

--- doc/tb_order_err_log.sql
+++ doc/tb_order_err_log.sql
@@ -1,12 +1,32 @@
 # 批量删除
 DELETE FROM `eshop_log`.`tb_order_err_log`
 WHERE `err_describe` LIKE '新增 订单过滤后为空！%'
-    LIMIT 10000;
+    LIMIT 100000;
 
 # 批量删除
 DELETE FROM `eshop_log`.`tb_order_err_log`
 WHERE `err_describe` LIKE '更新 订单过滤后为空！%'
-    LIMIT 10000;
+    LIMIT 100000;
+
+# 批量删除
+DELETE FROM `eshop_log`.`tb_order_err_log`
+WHERE `err_describe` LIKE 'shopLocation:%'
+    LIMIT 100000;
+
+# 批量删除
+DELETE FROM `eshop_log`.`tb_order_err_log`
+WHERE `err_describe` LIKE '订单同步，店铺必须设置店长！%'
+    LIMIT 100000;
+
+# 批量删除
+DELETE FROM `eshop_log`.`tb_order_err_log`
+WHERE `err_describe` LIKE '(add)(list-filter)(biz:order) filtered result is empty!%'
+    LIMIT 100000;
+
+# 批量删除
+DELETE FROM `eshop_log`.`tb_order_err_log`
+WHERE `err_describe` LIKE '(update)(list-filter)(biz:order) filtered result is empty!%'
+    LIMIT 100000;
 
 # 批量删除
 DELETE FROM `eshop_log`.`tb_order_err_log`
@@ -16,4 +36,5 @@ WHERE `err_describe` LIKE 'shopLocation:%'
 # 批量删除
 DELETE FROM `eshop_log`.`tb_order_err_log`
 WHERE `err_describe` LIKE '订单同步，店铺必须设置店长！%'
-    LIMIT 10000;
+    LIMIT 100000;
+
